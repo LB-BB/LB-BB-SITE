708fece95726c41e289d72061ff83236453e9223
LB <laurabradburn27@gmail.com>
2021-03-28 17:05:41 -0400

    Navbar, Hero, Home: some styling & functionality

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
-<<<<<<< HEAD
 import React from "react";
 import "./App.css";
-
-function App() {
-  return (
-    <div>
-      <h1>Oh hello</h1>
-    </div>
-=======
-import React from 'react';
-import Navbar from './components/Navbar';
-import './App.css';
-import Home from './components/pages/Home';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Services from './components/pages/Services';
-import Products from './components/pages/Products';
-import SignUp from './components/pages/SignUp';
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
 
 function App() {
   return (
@@ -23,14 +10,10 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/services' component={Services} />
-          <Route path='/products' component={Products} />
-          <Route path='/sign-up' component={SignUp} />
+          <Route path="/" exact component={Home} />
         </Switch>
       </Router>
     </>
->>>>>>> 163f81957ac5e32eb01a114a759d3ac4a396e0a9
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,79 @@
 import { Link } from "react-router-dom";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import "./Navbar.css";
+import Button from "./Button";
 
 function Navbar() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => {
+    setClick(!click);
+  };
+  const closeMobileMenu = () => setClick(false);
+
+  //this function displays the button based on screen size.
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  window.addEventListener("resize", showButton);
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          <Link to="/" className="navbar-logo"></Link>
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            LB-BB <i className="fas fa-brain"></i>
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+          {/* the above changes the burger icon to x and back.  */}
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            {/* the above expands/contracts the menu.  */}
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/services"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Projects
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/products"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Bio
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/sign-up"
+                className="nav-links-mobile"
+                onClick={closeMobileMenu}
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+          {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
         </div>
       </nav>
     </>
