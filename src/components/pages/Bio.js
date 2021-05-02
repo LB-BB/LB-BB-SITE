feb4229591379f07495511937dab677cce9892f5
LB <laurabradburn27@gmail.com>
2021-05-02 17:47:36 -0400

    Update: bio

--- src/components/pages/Bio.js
+++ src/components/pages/Bio.js
@@ -20,21 +20,23 @@ export default function Bio() {
               room creator. (Hint for when they reopen: colors always mean
               something!)
               <br></br>
-              <br></br>When I went looking for the intersection of artistry and
-              problem-solving, I found it in software engineering. I became a
-              developer in the Grace Hopper Program at Fullstack Academy, where
-              I trained in with Javascript, Git, and numerous libraries
-              including React. I earned a spot in Fullstack's Teaching
-              Fellowship after graduationg, and I currently mentor junior
-              engineers.
+              <br></br>When I went looking for an intersection of artistry and
+              problem-solving that could be done in quarantine, I found software
+              engineering. I became a developer in the Grace Hopper Program at
+              Fullstack Academy, where I trained in with Javascript, Git, and
+              numerous libraries including React. I earned a spot in Fullstack's
+              Teaching Fellowship after graduationg, and I currently mentor
+              junior engineers.
               <br></br>
               <br></br>I have also studied sustainability, and it remains an
               important interest of mine. It is my hope to one day apply my
               engineering skills to an aspect of marine ecology (hence, the
-              website theme). For now, I'm looking for a team that believes not
-              only in their end product, but in ensuring the quality of the work
-              from start to finish. As an escape room champ, I care a lot about
-              details.
+              website theme).
+              <br></br>
+              <br></br>
+              For now, I'm looking for a team that believes not only in their
+              end product, but in ensuring the quality of the work from start to
+              finish. As an escape room champ, I care a lot about details.
               <br></br>
               <br></br>
               <br></br>I have also seen 500+ films. Let's discuss your favorite!
